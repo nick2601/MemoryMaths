ce344dada4a26d2aaf5a12f0a842b2fe1dd17b8c
nick2601 <59530213+nick2601@users.noreply.github.com>
2025-09-27 03:53:53 +0530

    changes in gradle

---
+++ lib/overrides/figma_squircle/CHANGELOG.md
@@ -0,0 +1,15 @@
+## 0.5.3
+
+* Adding border align.
+
+## 0.5.1
+
+* Fixed image
+
+## 0.5.1
+
+* Added image
+
+## 0.5.0
+
+* Initial version
---
+++ lib/overrides/figma_squircle/lib/src/smooth_rectangle_border.dart
@@ -0,0 +1,195 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'smooth_border_radius.dart';
+
+enum BorderAlign {
+  inside,
+  center,
+  outside,
+}
+
+class SmoothRectangleBorder extends OutlinedBorder {
+  const SmoothRectangleBorder({
+    BorderSide side = BorderSide.none,
+    this.borderRadius = SmoothBorderRadius.zero,
+    this.borderAlign = BorderAlign.inside,
+  }) : super(side: side);
+
+  /// The radius for each corner.
+  ///
+  /// Negative radius values are clamped to 0.0 by [getInnerPath] and
+  /// [getOuterPath].
+  final SmoothBorderRadius borderRadius;
+  final BorderAlign borderAlign;
+
+  @override
+  EdgeInsetsGeometry get dimensions => EdgeInsets.all(side.width);
+
+  @override
+  ShapeBorder scale(double t) {
+    return SmoothRectangleBorder(
+      side: side.scale(t),
+      borderRadius: borderRadius * t,
+    );
+  }
+
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is SmoothRectangleBorder) {
+      return SmoothRectangleBorder(
+        side: BorderSide.lerp(a.side, side, t),
+        borderRadius: SmoothBorderRadius.lerp(a.borderRadius, borderRadius, t)!,
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is SmoothRectangleBorder) {
+      return SmoothRectangleBorder(
+        side: BorderSide.lerp(side, b.side, t),
+        borderRadius: SmoothBorderRadius.lerp(borderRadius, b.borderRadius, t)!,
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    final innerRect = () {
+      switch (borderAlign) {
+        case BorderAlign.inside:
+          return rect.deflate(side.width);
+        case BorderAlign.center:
+          return rect.deflate(side.width / 2);
+        case BorderAlign.outside:
+          return rect;
+      }
+    }();
+    final radius = () {
+      switch (borderAlign) {
+        case BorderAlign.inside:
+          return borderRadius -
+              SmoothBorderRadius.all(
+                SmoothRadius(
+                  cornerRadius: side.width,
+                  cornerSmoothing: 1.0,
+                ),
+              );
+        case BorderAlign.center:
+          return borderRadius -
+              SmoothBorderRadius.all(
+                SmoothRadius(
+                  cornerRadius: side.width / 2,
+                  cornerSmoothing: 1.0,
+                ),
+              );
+        case BorderAlign.outside:
+          return borderRadius;
+      }
+    }();
+
+    if ([radius.bottomLeft, radius.bottomRight, radius.topLeft, radius.topRight]
+        .every((x) => x.cornerSmoothing == 0.0)) {
+      return Path()..addRRect(radius.resolve(textDirection).toRRect(innerRect));
+    }
+
+    return radius.toPath(innerRect);
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    final outerRect = () {
+      switch (borderAlign) {
+        case BorderAlign.inside:
+          return rect;
+        case BorderAlign.center:
+          return rect.inflate(side.width / 2);
+        case BorderAlign.outside:
+          return rect.inflate(side.width);
+      }
+    }();
+    final radius = () {
+      switch (borderAlign) {
+        case BorderAlign.inside:
+          return borderRadius;
+        case BorderAlign.center:
+          return borderRadius +
+              SmoothBorderRadius.all(
+                SmoothRadius(
+                  cornerRadius: side.width / 2,
+                  cornerSmoothing: 1.0,
+                ),
+              );
+        case BorderAlign.outside:
+          return borderRadius +
+              SmoothBorderRadius.all(
+                SmoothRadius(
+                  cornerRadius: side.width,
+                  cornerSmoothing: 1.0,
+                ),
+              );
+      }
+    }();
+
+    if ([radius.bottomLeft, radius.bottomRight, radius.topLeft, radius.topRight]
+        .every((x) => x.cornerSmoothing == 0.0)) {
+      return Path()..addRRect(radius.resolve(textDirection).toRRect(outerRect));
+    }
+
+    return radius.toPath(outerRect);
+  }
+
+  @override
+  SmoothRectangleBorder copyWith({
+    BorderSide? side,
+    SmoothBorderRadius? borderRadius,
+    BorderAlign? borderAlign,
+  }) {
+    return SmoothRectangleBorder(
+      side: side ?? this.side,
+      borderRadius: borderRadius ?? this.borderRadius,
+      borderAlign: borderAlign ?? this.borderAlign,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    if (rect.isEmpty) return;
+    switch (side.style) {
+      case BorderStyle.none:
+        break;
+      case BorderStyle.solid:
+        final outerPath = getOuterPath(
+          rect,
+          textDirection: textDirection,
+        );
+
+        final paint = side.toPaint();
+
+        canvas.drawPath(outerPath, paint);
+
+        break;
+    }
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) return false;
+    return other is SmoothRectangleBorder &&
+        other.side == side &&
+        other.borderRadius == borderRadius &&
+        other.borderAlign == borderAlign;
+  }
+
+  @override
+  int get hashCode => Object.hash(side, borderRadius, borderAlign);
+
+  @override
+  String toString() {
+    return '${objectRuntimeType(this, 'SmoothRectangleBorder')}($side, $borderRadius, $borderAlign)';
+  }
+}
